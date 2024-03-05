7489bbd9a10671f4ebd59f5d886bb1fe93885357
gstvcampos <gustavo.bmc76@gmail.com>
2024-03-05 06:46:49 -0300

    feat: cart respansive

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
+import { auth } from '@/auth'
 import { cn } from '@/lib/ultis'
 import type { Metadata } from 'next'
 import { SessionProvider } from 'next-auth/react'
 import { Montserrat } from 'next/font/google'
-import { auth } from '@/auth'
 import './globals.css'
 
 const mont = Montserrat({
@@ -26,7 +26,9 @@ export default async function RootLayout({
     <SessionProvider session={session}>
       <html lang="pt-br" className="!scroll-smooth">
         <body className={cn(mont.className, 'font-sans antialiased')}>
-          <main className="relative flex flex-col min-h-dvh">{children}</main>
+          <main className="relative flex flex-col min-h-dvh overflow-hidden">
+            {children}
+          </main>
         </body>
       </html>
     </SessionProvider>
