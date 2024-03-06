2aed75fb348c45dbc45aa72ca4f02fdda05e29b8
gstvcampos <gustavo.bmc76@gmail.com>
2024-03-06 13:09:57 -0300

    feat: style add product form

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,14 +1,11 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
-import Banner from '@/components/sliders/Banner'
 import CarouselProducts from '@/components/sliders/CarouselProducts'
 import { prisma } from '@/db/prisma'
 
 export default async function Home() {
   const products = await prisma.product.findMany()
-  const slides = ['/estadio.webp', '/estadio1.webp', '/estadio2.webp']
   return (
     <>
-      <Banner slides={slides} />
       <MaxWidthWrapper>
         <CarouselProducts products={products} title="lançamentos" />
         <CarouselProducts products={products} title="masculino" />
