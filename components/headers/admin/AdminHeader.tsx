1fc2049abb7c1e8a274fba450cd2ffc962c4471f
gstvcampos <gustavo.bmc76@gmail.com>
2024-03-06 11:51:23 -0300

    feat: admin header style

--- components/headers/admin/AdminHeader.tsx
+++ components/headers/admin/AdminHeader.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function AdminHeader() {
   return (
-    <header className="bg-gray-800 py-4">
+    <header className="navbar bg-secondary text-primary-content">
       <nav className="container mx-auto flex items-center justify-between">
         <div className="flex items-center">
           <Link href="/admin">Pagina do administrador</Link>
@@ -14,6 +14,9 @@ export default function AdminHeader() {
           </li>
           <li>
             <Link href="/admin/produtos">Produtos</Link>
+          </li>
+          <li>
+            <Link href="/admin/clientes">Clientes</Link>
           </li>
         </ul>
       </nav>
