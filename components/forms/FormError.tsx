de1ae0b0f172b821b6a45d1b56555adb639baded
gstvcampos <gustavo.bmc76@gmail.com>
2024-03-15 09:05:10 -0300

    feat: email resend

--- components/forms/FormError.tsx
+++ components/forms/FormError.tsx
@@ -4,7 +4,7 @@ export default function FormError({ message }: { message?: string }) {
   if (!message) return null
 
   return (
-    <div className="bg-error-content p-3 rounded-md flex items-center gap-x-2 text-sm text-error">
+    <div className="bg-red-500/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-red-500 my-4">
       <ExclamationTriangleIcon className="h-4 w-4" />
       <p>{message}</p>
     </div>
--- components/forms/FormSuccess.tsx
+++ components/forms/FormSuccess.tsx
@@ -4,7 +4,7 @@ export default function FormSuccess({ message }: { message?: string }) {
   if (!message) return null
 
   return (
-    <div className="bg-emerald-500/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-emerald-500">
+    <div className="bg-emerald-500/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-emerald-500 my-4">
       <CheckCircleIcon className="h-4 w-4" />
       <p>{message}</p>
     </div>
