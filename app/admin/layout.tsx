444b6263f4937d5284049686596feaaef0ccbf2c
gstvcampos <gustavo.bmc76@gmail.com>
2024-03-15 17:44:41 -0300

    feat: admin redirect

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,14 +1,21 @@
+import { auth } from '@/auth'
 import AdminHeader from '@/components/headers/admin/AdminHeader'
+import { redirect } from 'next/navigation'
 
 export default async function AdminLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <>
-      <AdminHeader />
-      <div className="flex-grow flex-1">{children}</div>
-    </>
-  )
+  const session = await auth()
+  if (session?.user.role !== 'ADMIN') {
+    redirect('/')
+  } else {
+    return (
+      <>
+        <AdminHeader />
+        <div className="flex-grow flex-1">{children}</div>
+      </>
+    )
+  }
 }
