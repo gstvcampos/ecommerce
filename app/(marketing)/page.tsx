b68fb3fec9d8e30836afc97d65f884ea17234857
gstvcampos <gustavo.bmc76@gmail.com>
2024-03-15 17:56:00 -0300

    feat: banner

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,13 +1,16 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import Banner from '@/components/sliders/Banner'
 import CarouselProducts from '@/components/sliders/CarouselProducts'
 import { prisma } from '@/db/prisma'
 
 export default async function Home() {
   const products = await prisma.product.findMany()
   const firstEightProducts = products.slice(0, 8)
+  const slides = ['/banner1.webp', '/banner2.webp', '/banner3.webp']
 
   return (
     <>
+      <Banner slides={slides} />
       <MaxWidthWrapper>
         <CarouselProducts products={firstEightProducts} title="lançamentos" />
         <CarouselProducts products={firstEightProducts} title="masculino" />
