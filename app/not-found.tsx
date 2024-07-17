ae2bbd9e8d9da619a57ea4c08d62d86ed787a68c
gstvcampos <gustavo.bmc76@gmail.com>
2024-07-17 11:01:54 -0300

    feat: not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,22 @@
+import Header from '@/components/headers/marketing/Header'
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function NotFound() {
   return (
-    <div>
-      <h1 className="">pagina nao encontrada</h1>
-    </div>
+    <main className="relative min-h-dvh flex flex-col">
+      <Header />
+      <div className="flex flex-col items-center gap-10 pt-16">
+        <Image
+          alt="imagen pagina nao econtrada"
+          src="404-page-not-found.svg"
+          width={300}
+          height={300}
+        />
+        <Link className="font-bold btn-lg btn" href="/">
+          Voltar à página inicial
+        </Link>
+      </div>
+    </main>
   )
 }
